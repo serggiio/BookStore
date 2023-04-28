607a4b881c357170af5db626be6e972ba5fbec92
serggiio <36702285+serggiio@users.noreply.github.com>
2023-04-28 10:42:42 -0400

    Add project entities

---
+++ BookStore.Infrastructure/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Entities
+{
+    public class Author
+    {
+        public int Id { get; }
+
+        public string? Name { get; init; }
+
+        public string? Country { get; init; }
+
+    }
+}
---
+++ BookStore.Infrastructure/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Entities
+{
+    public class Book
+    {
+        public int Id { get; }
+
+        public string? Title { get; set; }
+
+        public int? Pages { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+
+        public DateTime? ModificationDate { get; set; }
+    }
+}
