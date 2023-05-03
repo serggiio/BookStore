e05175fe0ddf8694bc9c5a0da951cd9393b85f82
serggiio <36702285+serggiio@users.noreply.github.com>
2023-05-03 00:30:07 -0400

    Add validator injection and middleware exception

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -3,12 +3,18 @@ using BookStore.Domain.Interfaces;
 using BookStore.Infrastructure.Entities;
 using BookStore.Infrastructure.Repositories;
 using BookStore.Infrastructure.Interfaces;
+using BookStore.API.Extensions;
+using FluentValidation.AspNetCore;
+using BookStore.Domain.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers()
+                .AddNewtonsoftJson()
+                .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<BookValidator>())
+                .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<AuthorValidator>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,7 +25,16 @@ SQLConnection sqlConnection = builder.Configuration.GetSection("SQLConnection").
 builder.Services.AddScoped<IBaseRepository<Author>>(x => new AuthorRepository(sqlConnection));
 builder.Services.AddScoped<IBaseRepository<Book>>(x => new BookRepository(sqlConnection));
 
+builder.Services.UseCustomBadExceptionHandler();
+
 var app = builder.Build();
+app.UseCustomExceptionHandler();
+
+app.Use(async (context, next) =>
+{
+    context.Request.EnableBuffering();
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
